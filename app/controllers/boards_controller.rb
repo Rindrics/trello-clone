e72562476a67e0de8be6380fa0048657e5105250
Akira Hayashi <smxshxishxad@me.com>
2024-02-09 06:55:01 +0900

    Create board using form

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -10,4 +10,19 @@ class BoardsController < ApplicationController
   def new
     @board = Board.new
   end
+
+  def create
+    @board = Board.new(board_params)
+
+    if @board.save
+      redirect_to @board
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def board_params
+      params.require(:board).permit(:name)
+    end
 end
