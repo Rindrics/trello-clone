1794131930666c7ef5c07173847749648909a53a
Akira Hayashi <smxshxishxad@me.com>
2024-02-12 04:52:31 +0900

    Separate 'create' and 'update' of list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,8 @@
 class ListsController < ApplicationController
   def create
     @board = Board.find(params[:board_id])
-    @list = @board.lists.create(list_params)
+    @list = @board.lists.create(list_params.merge(status: 'active'))
+
     redirect_to board_path(@board)
   end
 
